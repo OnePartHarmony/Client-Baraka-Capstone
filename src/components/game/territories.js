da1df3ab239aca142b03df1331544688f13e3b4a
OnePartHarmony <bakingharmony@gmail.com>
2022-11-22 13:50:41 -0500

    hex images are properly sized

--- src/components/game/territories.js
+++ src/components/game/territories.js
@@ -39,7 +39,7 @@ const mapTerritories = (territories, clickFunction) => {
                             'id': `${territory.number}`,
                             'name': `${territory.number}`,
                             'shape': 'poly',
-                            'coords': [50,0,100,30,100,90,50,120,0,90,0,30]
+                            'coords': [50,0,100,28.5,100,85.5,50,114,0,85.5,0,28.5]
                             // strokeColor: rgb value for outline to represent ownership
                         }]                        
                     }
@@ -47,12 +47,12 @@ const mapTerritories = (territories, clickFunction) => {
         territoriesJSX.splice(territory.number, 1, (
             <div
                 key={territory.number}
-                style={{backgroundImage: `url(${background})`, height: '120px', width: '100px', backgroundSize: '100px 120px'}}
+                style={{backgroundImage: `url(${background})`, height: '114px', width: '100px', backgroundSize: '100px 114px'}}
             >
                 <ImageMapper
                     src={'https://i.imgur.com/oAra3xY.png'}
                     map= {map}
-                    height='120'
+                    height='114'
                     width='100'
                     onClick={clickFunction}
                 />
@@ -68,22 +68,22 @@ console.log(territoriesJSX)
             <div className='hexRow1' style={{margin: 'auto', display: 'flex', flexDirection: 'row'}}>
                 {territoriesJSX.slice(0,4)}
             </div>
-            <div className='hexRow2' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-35px'}}>
+            <div className='hexRow2' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-28px'}}>
                 {territoriesJSX.slice(4,9)}
             </div>
-            <div className='hexRow3' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-70px'}}>
+            <div className='hexRow3' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-56px'}}>
                 {territoriesJSX.slice(9,15)}
             </div>
-            <div className='hexRow4' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-105px'}}>
+            <div className='hexRow4' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-84px'}}>
                 {territoriesJSX.slice(15,22)}
             </div>
-            <div className='hexRow5' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-140px'}}>
+            <div className='hexRow5' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-112px'}}>
                 {territoriesJSX.slice(22,28)}
             </div>
-            <div className='hexRow6' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-175px'}}>
+            <div className='hexRow6' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-140px'}}>
                 {territoriesJSX.slice(28,33)}
             </div>
-            <div className='hexRow7' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-210px'}}>
+            <div className='hexRow7' style={{margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', top: '-168px'}}>
                 {territoriesJSX.slice(33,37)}
             </div>
         </div>
