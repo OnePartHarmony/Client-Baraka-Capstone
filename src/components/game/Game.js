99c68eab36ab76ff7afa8ba83e107fbcdd187018
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2022-11-26 15:43:15 -0500

    Update Game.js
    removed some unused code

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -5,12 +5,10 @@ import ActionMenu from './ActionMenu'
 
 const Game = (props) => {
 
-    const [updated, setUpdated] = useState(false)
-
     const {user} = props
 
     return (
-        <div className='game'  onresize={() => setUpdated(prev => !prev)}>
+        <div className='game'>
             <div className='gameLeft'>
                 <GameBoard user={user}/>
                 <StatusBar user={user}/>
