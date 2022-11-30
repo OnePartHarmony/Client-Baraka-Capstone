2f8b91446cc7c1b76b486f99ed12a61e0a85265e
iceagelemonade <bryanjcarr88@gmail.com>
2022-11-30 13:32:58 -0500

    fixing to get most recent pull

--- src/components/game/game-board/Territory.js
+++ src/components/game/game-board/Territory.js
@@ -3,17 +3,39 @@ import ImageMapper from 'react-img-mapper'
 import { setTerritoryBackground, setSoldier, setPriest } from './setTerritoryImages'
 import invisible from '../../../images/invisible.png'
 import peasant from '../../../images/onePeasant.png'
+import { checkClickable } from './checkClickable'
 
 const Territory = (props) => {
 
-    const {user, territory, hexWidth, setClickedTerritory} = props
-
+    const {gameObject, userPlayerObject, territory, hexWidth, clickedTerritory, setClickedTerritory, clickableBoard} = props
+    const [clickable, setClickable] = useState(false)
+    
     const background = setTerritoryBackground(territory)
     const soldier = setSoldier(territory)
     const priest = setPriest(territory)
+    
+    
+    useEffect(()=> {
+        setClickable(checkClickable(territory, clickableBoard, gameObject, userPlayerObject))        
+    }, [clickableBoard])
 
-    const clickFunction = (e) => {
-        setClickedTerritory(e.id)
+
+    const toggleClickedTerritory = () => {        
+        if (clickable){
+            setClickedTerritory(previousClick => {
+                if (previousClick != territory.number){
+                    setClickedTerritory(territory.number)
+                } else {
+                    setClickedTerritory('')
+                }
+            })           
+        }        
+    }
+
+    let fillColor = 'rgba(255, 255, 255, 0.5)'    
+    if (!clickable) {
+        console.log('here')
+        fillColor = 'rgba(255, 255, 255, 0)'
     }
 
     const map = {
@@ -24,6 +46,8 @@ const Territory = (props) => {
             'shape': 'poly',
             'coords': [50,0,100,28.5,100,85.5,50,114,0,85.5,0,28.5],
             'stayHighlighted': 'true',
+            'fillColor': `${fillColor}`,
+            'preFillColor': 'rgba(255,255,0,0)'            
         }]
     }
 
@@ -32,20 +56,19 @@ const Territory = (props) => {
         <div
             key={territory.number}
             style={{backgroundImage: `url(${background})`, height: 1.14 * hexWidth, width: hexWidth, backgroundSize: '100% 100%'}}
-        >
-            {territory.type != 'water' && 
-                <ImageMapper style={{zIndex: 2}}
-                    areaKeyName={territory.number}
-                    src={invisible}
-                    map={map}
-                    onClick={clickFunction}
-                    responsive= 'true'
-                    width={hexWidth}
-                    imgWidth={100}
-                    parentWidth={hexWidth}
-                    stayHighlighted
-                /> 
-            }
+        >            
+            <ImageMapper style={{zIndex: 2}}
+                areaKeyName={territory.number}
+                src={invisible}
+                map={map}
+                onClick={toggleClickedTerritory}
+                responsive= 'true'
+                width={hexWidth}
+                imgWidth={100}
+                parentWidth={hexWidth}
+                stayHighlighted={true}
+                toggleHighlighted={true}
+            />              
             <div className="territoryImages">
                 {territory.soldiers > 0 && 
                     <><img className="territoryDude" src={soldier} alt='soldier'/><strong>x {territory.soldiers}</strong><br/></>
@@ -59,7 +82,7 @@ const Territory = (props) => {
                     <><img className="territoryDude" src={peasant} alt='soldier'/><strong>x {territory.population}</strong></>
                 }
             </div>
-                      
+                   
             {/* render display of units and properties for territory */}
         </div>               
     )
